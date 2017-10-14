77555d8bfff26f7bd4142b1343e79bcfa47f22fa
matt_haynes <m0hayn01@cardmail.louisville.edu>
2017-10-14 16:24:21 -0400

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         myDb = new DatabaseHelper(this); //will call constructor of DataBaseHelper class
 
+        /* Buttons and Fields are stupid. You have to Cast them so they know what they are (as used here) */ 
         /* Casting buttons and Text fields located in UI, button/field ids found in activity_main.xml*/
         editDEPT= (EditText)findViewById(R.id.editText_dept); //this looks-for/assigns button by id found in activity_main.xml
         editCLASS= (EditText)findViewById(R.id.editText_class);
