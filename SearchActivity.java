930b295eb50eabb558264a1a5f3c97dc62e465f2
matt_haynes <m0hayn01@cardmail.louisville.edu>
2017-10-23 20:33:19 -0400

    Create SearchActivity.java

---
+++ SearchActivity.java
@@ -0,0 +1,20 @@
+package com.soc.matthewhaynes.sqliteapp;
+
+/**********************************  CUT BELOW HERE TO PASTE *****************************************************/
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);       //auto-generated
+        setContentView(R.layout.activity_search); //auto-generated
+
+        /*    CODE HERE    */
+
+
+    }
+}
