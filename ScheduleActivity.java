899aefba48a18d16bb5cf89a5926315a9f4007d4
matt_haynes <m0hayn01@cardmail.louisville.edu>
2017-10-23 20:32:42 -0400

    Create ScheduleActivity.java

---
+++ ScheduleActivity.java
@@ -0,0 +1,21 @@
+package com.soc.matthewhaynes.sqliteapp; //this is different depending on the PATH to your project. remember to CLEAN -> REBUILD when downloading
+
+/**********************************  CUT BELOW HERE TO PASTE *****************************************************/
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);         //auto-generated
+        setContentView(R.layout.activity_schedule); //auto-generated
+
+
+        /*  CODE HERE */
+
+
+
+    }
+}
