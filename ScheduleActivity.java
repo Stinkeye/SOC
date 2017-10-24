35d4e2b3f31681bdea2c1236b45ec8df559b6ae4
matt_haynes <m0hayn01@cardmail.louisville.edu>
2017-10-23 21:10:04 -0400

    Update ScheduleActivity.java

--- ScheduleActivity.java
+++ ScheduleActivity.java
@@ -7,15 +7,25 @@ import android.os.Bundle;
 
 public class ScheduleActivity extends AppCompatActivity {
 
+    /* VARS HERE */
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);         //auto-generated
         setContentView(R.layout.activity_schedule); //auto-generated
 
 
-        /*  CODE HERE */
+        /*  BUTTON AND TXT FIELDS CODE HERE */
+        
+        
+        
+        /* CALL METHODS HERE */
 
 
 
     }
+    
+    /* METHOD CODE HERE */
+    
+    
 }
